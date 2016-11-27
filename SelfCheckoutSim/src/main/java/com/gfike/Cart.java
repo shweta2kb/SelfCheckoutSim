af190066a3cdb0f3c5edcdb926799de8af27a278
gfike19 <gfike19@gmail.com>
2016-11-27 11:19:58 -0600

    did things

--- SelfCheckoutSim/src/main/java/com/gfike/Cart.java
+++ SelfCheckoutSim/src/main/java/com/gfike/Cart.java
@@ -1,5 +1,23 @@
 package com.gfike;
 
-public class Cart {
+import java.util.ArrayList;
 
+public class Cart {
+	ArrayList<Product> lst = new ArrayList<Product>();
+	
+	public Cart () {}
+	
+	public void addProduct(Product p) {
+		lst.add(p);
+	}
+	
+	public void removeProduct(Product p) {
+		lst.remove(p);
+	}
+	
+	public void addMultiples(int num, Product p){
+		for(int i = 0; i < num -1; i ++) {
+			lst.add(p);
+		}
+	}
 }
