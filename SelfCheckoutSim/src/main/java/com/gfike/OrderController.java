3cfc3b6dc52d8630b8395c998c0dae5d3eba2854
gfike19 <gfike19@gmail.com>
2016-11-27 21:15:55 -0600

    need to test data list in newOrder. started order controller

---
+++ SelfCheckoutSim/src/main/java/com/gfike/OrderController.java
@@ -0,0 +1,23 @@
+package com.gfike;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class OrderController {
+
+	@RequestMapping(value="/newOrder", method=RequestMethod.GET)
+	public String newOrder (){
+		return "newOrder";
+	}
+	
+	@RequestMapping(value="/newOrder",method=RequestMethod.POST)
+	public String newOrder(HttpServletRequest request, Model model) {
+		return null;
+		
+	}
+}
